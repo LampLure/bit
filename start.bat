af2b0e4265ace5f0846e14b586299a95710da176
LampLure <lamplure@users.noreply.github.com>
2026-05-17 20:41:47 +0800

    fix: set ELECTRON_MIRROR to npmmirror.com for China mainland users

--- start.bat
+++ start.bat
@@ -1,6 +1,8 @@
 @echo off
 cd /d %~dp0
 
+set ELECTRON_MIRROR=https://npmmirror.com/mirrors/electron/
+
 if not exist node_modules (
   echo [start] Installing dependencies...
   npm install
--- start.sh
+++ start.sh
@@ -3,6 +3,8 @@ set -e
 
 cd "$(dirname "$0")"
 
+export ELECTRON_MIRROR="${ELECTRON_MIRROR:-https://npmmirror.com/mirrors/electron/}"
+
 if [ ! -d node_modules ]; then
   echo "[start] Installing dependencies..."
   npm install
